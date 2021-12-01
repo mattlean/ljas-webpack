80f8ead94bb609335685cf2a8c4c789704670dc3
Matthew Lean <matt@mattlean.com>
2021-12-01 02:41:21 -0800

    add babel

--- dist/index.js
+++ dist/index.js
@@ -19,6 +19,29 @@ exports.cleanOutput = (clean = true) => ({
 })
 
 /**
+ * Transpile JavaScript files using Babel with babel-loader.
+ * @param {Object} [argObj] Argument object
+ * @param {Object} [argObj.rule] webpack rule. Overrides styleLoaderOptions & cssLoaderOptions when a conflict occurs.
+ * @param {RegExp} [argObj.rule.test=/\.m?js$/i] webpack rule test assertion
+ * @param {Object} [argObj.options] babel-loader options
+ * @return {Object} babel-loader config
+ */
+exports.compileBabel = ({ rule = {}, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: (rule && rule.test) || /\.m?js$/i,
+        use: {
+          loader: 'babel-loader',
+          options,
+        },
+        ...rule,
+      },
+    ],
+  },
+})
+
+/**
  * Setup webpack-dev-server.
  * @param {Object} [devServer] webpack-dev-server options
  * @return {Object} devServer config
@@ -28,7 +51,7 @@ exports.setupDevServer = (devServer) => ({ devServer })
 /**
  * Handle CSS and inject into the DOM.
  * @param {Object} [argObj] Argument object
- * @param {Object} [argObj.rule] webpack rule
+ * @param {Object} [argObj.rule] webpack rule. Overrides styleLoaderOptions & cssLoaderOptions when a conflict occurs.
  * @param {RegExp} [argObj.rule.test=/\.css$/i] webpack rule test assertion
  * @param {Object} [argObj.styleLoaderOptions] style-loader options
  * @param {Object} [argObj.cssLoaderOptions] css-loader options
@@ -42,12 +65,12 @@ exports.injectCSS = ({
   module: {
     rules: [
       {
-        ...rule,
         test: (rule && rule.test) || /\.css$/i,
         use: [
           { loader: 'style-loader', options: styleLoaderOptions },
           { loader: 'css-loader', options: cssLoaderOptions },
         ],
+        ...rule,
       },
     ],
   },
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
-const { buildHTML, cleanOutput, injectCSS, setupDevServer } = require('../dist')
+const {
+  buildHTML,
+  cleanOutput,
+  compileBabel,
+  injectCSS,
+  setupDevServer,
+} = require('../dist')
 
 module.exports = merge(
   {
     entry: path.resolve(__dirname, 'index.js'),
+
     output: {
       path: path.resolve(__dirname, 'public'),
       filename: 'script.js',
     },
+
     mode: 'development',
   },
+
+  compileBabel(),
 
   buildHTML(),
 
